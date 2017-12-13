ddf8078b453f346216eff91963df29fb88de56dc
Tatsuya Takamura <ttakamura@preferred.jp>
2017-12-13 23:49:52 +0900

    Fix matplotlib

--- pose_detector.py
+++ pose_detector.py
@@ -2,6 +2,8 @@ import cv2
 import time
 import argparse
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
 
